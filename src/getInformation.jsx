2b656f213fa888f5beb8292a123a433070c8bae8
graganold <geordinraganold@gmail.com>
2025-05-05 14:12:29 -0400

    move data to public folder

---
+++ src/getInformation.jsx
@@ -0,0 +1,10 @@
+export default function WeekSelection() {
+	return (
+		<>
+			<label for="week">Choose a Curling Zone ranking week:</label>
+			<select name="week" id="week">
+				
+			</select>
+		</>
+	)
+}
