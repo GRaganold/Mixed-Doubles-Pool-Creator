bf416cb87f6cb452c9990a477efaa92eabc2c55a
graganold <geordinraganold@gmail.com>
2025-05-07 15:06:38 -0400

    feat(ui): add Prettier support and format entire project

--- src/getInformation.jsx
+++ src/getInformation.jsx
@@ -1,10 +1,8 @@
 export default function WeekSelection() {
-	return (
-		<>
-			<label for="week">Choose a Curling Zone ranking week:</label>
-			<select name="week" id="week">
-				
-			</select>
-		</>
-	)
+  return (
+    <>
+      <label for="week">Choose a Curling Zone ranking week:</label>
+      <select name="week" id="week"></select>
+    </>
+  );
 }
